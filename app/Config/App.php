48905bd9539cbafe5f873864d58ea40caa3b63a3
Andrew Bagus <bagus.andrew@gmail.com>
2022-11-10 21:43:41 +0700

    fix config base url

--- app/Config/App.php
+++ app/Config/App.php
@@ -24,7 +24,7 @@ class App extends BaseConfig
    *
    * @var string
    */
-  public $baseURL = BASE === null ? 'http://localhost:8000/' : BASE;
+  public $baseURL = BASE === '' ? 'http://localhost:8000/' : BASE;
 
   /**
    * --------------------------------------------------------------------------
