1f0af4883e81c2a3d436386e324b3873f5bca317
Andrew Bagus <bagus.andrew@gmail.com>
2022-11-10 20:21:01 +0700

    fix typo

--- app/Config/App.php
+++ app/Config/App.php
@@ -24,8 +24,7 @@ class App extends BaseConfig
    *
    * @var string
    */
-  // public $baseURL = BASE === null ? '' : BASE;
-  public $baseURL = "http://localhost:8000/";
+  public $baseURL = BASE === null ? 'http://localhost:8000/' : BASE;
 
   /**
    * --------------------------------------------------------------------------
