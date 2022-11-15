807b5a3dd8919243a94be97ae324a6a765c47356
Andrew Bagus <bagus.andrew@gmail.com>
2022-11-15 21:31:45 +0700

    fix poper and bootstrap5 js

--- app/Views/layouts/js.php
+++ app/Views/layouts/js.php
@@ -1,5 +1,7 @@
 <script type="text/javascript" src="<?= base_url() ?>/assets/vendors/jquery/jquery.min.js"></script>
-<script src="https://kit.fontawesome.com/450de19a46.js" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/2.9.2/umd/popper.min.js" integrity="sha512-2rNj2KJ+D8s1ceNasTIex6z4HWyOnEYLVC3FigGOmyQCZc2eBXKgOxQmo3oKLHyfcj53uz4QMsRCWNbLd32Q1g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.2.2/js/bootstrap.min.js" integrity="sha512-5BqtYqlWfJemW5+v+TZUs22uigI8tXeVah5S/1Z6qBLVO7gakAOtkOzUtgq6dsIo5c0NJdmGPs0H9I+2OHUHVQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<script type="text/javascript" src="https://kit.fontawesome.com/450de19a46.js" crossorigin="anonymous"></script>
 <script type="text/javascript" src="<?= base_url() ?>/assets/vendors/momentjs/moment-with-locales.js"></script>
 <script type="text/javascript" src="<?= base_url() ?>/assets/vendors/tempusdominus/js/tempusdominus-bootstrap-4.js"></script>
 <script type="text/javascript" src="<?= base_url() ?>/assets/vendors/jquery-validation/jquery.validate.min.js"></script>
@@ -9,9 +11,6 @@
 <script type="text/javascript" src="<?= base_url() ?>/assets/vendors/summernote/summernote-bs4.js"></script>
 <script type="text/javascript" src="<?= base_url() ?>/assets/vendors/select2/js/select2.full.min.js"></script>
 <script type="text/javascript" src="<?= base_url() ?>/assets/vendors/datatables/datatables.min.js"></script>
-<script type="text/javascript" src="<?= base_url() ?>/assets/vendors/datatables/responsive/js/dataTables.responsive.min.js"></script>
-<script type="text/javascript" src="<?= base_url() ?>/assets/vendors/datatables/responsive/js/responsive.bootstrap5.min.js"></script>
-<script type="text/javascript" src="<?= base_url() ?>/assets/vendors/datatables/theme/js/dataTables.bootstrap5.min.js"></script>
 <script type="text/javascript" src="<?= base_url() ?>/assets/js/main.js"></script>
 <script type="text/javascript" src="<?= base_url() ?>/js/core/global.js?v=<?= rand() ?>"></script>
 <script type="text/javascript" src="<?= base_url() ?>/js/core/init.js?v=<?= rand() ?>"></script>
