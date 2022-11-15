e67abf51f863663afdc4e7e74298765f037bd524
Andrew Bagus <bagus.andrew@gmail.com>
2022-11-15 21:32:29 +0700

    add function dataTable

--- app/Helpers/My_helper.php
+++ app/Helpers/My_helper.php
@@ -16,3 +16,34 @@ function get_features($role_id)
   return $features;
 }
 
+function datatableQuery($query, $search, $column_search, $column_order, $order, $order_by)
+{
+  foreach ($column_search as $i => $item) // looping awal
+  {
+    if ($search['value']) // jika datatable mengirimkan pencarian dengan metode POST
+    {
+
+      if ($i === 0) // looping awal
+      {
+        $query = $query->groupStart();
+        $query = $query->like($item, $search['value']);
+      } else {
+        $query = $query->orLike($item, $search['value']);
+      }
+
+      if (count($column_search) - 1 == $i) {
+        $query = $query->groupEnd();
+      }
+    }
+    $i++;
+  }
+
+  if (isset($order)) {
+    $query = $query->orderBy($column_order[$order['0']['column']], $order['0']['dir']);
+  } else if (isset($order_by)) {
+    $query->order = $order_by;
+    $query = $query->orderBy(key($order), $order[key($order)]);
+  }
+
+  return $query;
+}
