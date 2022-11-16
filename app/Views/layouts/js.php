89229b393d449620c3c43359472c0d63d94c4e46
Andrew Bagus <bagus.andrew@gmail.com>
2022-11-16 11:39:19 +0700

    fix show dropdown menu

--- app/Views/layouts/js.php
+++ app/Views/layouts/js.php
@@ -1,5 +1,6 @@
 <script type="text/javascript" src="<?= base_url() ?>/assets/vendors/jquery/jquery.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.2/js/bootstrap.bundle.min.js" integrity="sha512-BOsvKbLb0dB1IVplOL9ptU1EYA+LuCKEluZWRUYG73hxqNBU85JBIBhPGwhQl7O633KtkjMv8lvxZcWP+N3V3w==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.17.21/lodash.min.js" integrity="sha512-WFN04846sdKMIP5LKNphMaWzU7YpMyCU245etK3g/2ARYbPK9Ub18eG+ljU96qKRCWh+quCY7yefSmlkQw1ANQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 <script type="text/javascript" src="https://kit.fontawesome.com/450de19a46.js" crossorigin="anonymous"></script>
 <script type="text/javascript" src="<?= base_url() ?>/assets/vendors/momentjs/moment-with-locales.js"></script>
 <script type="text/javascript" src="<?= base_url() ?>/assets/vendors/tempusdominus/js/tempusdominus-bootstrap-4.js"></script>
--- public/js/core/init.js
+++ public/js/core/init.js
@@ -1,10 +1,10 @@
-$(function () {
-  $('.select2bs5').select2({
+$(function (w, d) {
+  $('.select2bs4').select2({
     theme: 'bootstrap-5',
     allowClear: true,
   })
 
-  $('.select2bs5-nonclear').select2({
+  $('.select2bs4-nonclear').select2({
     theme: 'bootstrap-5',
   })
 
@@ -15,10 +15,22 @@ $(function () {
   $('input[data-toggle=datetimepicker]').datetimepicker({
     format: date_format,
   })
-})
-window.addEventListener('load', function () {
-  const t = document.getElementById('loader')
-  setTimeout(function () {
-    t.classList.add('fadeOut')
-  }, 300)
+
+  $(document).on('click', '#dropdownMenuLink', function (e) {
+    e.preventDefault()
+    if ($(this).hasClass('show')) {
+      $(this).removeClass('show')
+      $(this).parents('li').find('ul.dropdown-menu').removeClass('show')
+    } else {
+      $(this).addClass('show')
+      $(this).parents('li').find('ul.dropdown-menu').addClass('show')
+    }
+  })
+
+  window.addEventListener('load', function () {
+    const t = document.getElementById('loader')
+    setTimeout(function () {
+      t.classList.add('fadeOut')
+    }, 300)
+  })
 })
