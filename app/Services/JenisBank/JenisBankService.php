8ebe0f6d725c904041bd821a1a3fe509a50440b8
Andrew Bagus <andrew.bagus@gmail.com>
2022-11-27 13:19:08 +0700

    standarize jenisBank call in services

--- app/Services/JenisBank/JenisBankService.php
+++ app/Services/JenisBank/JenisBankService.php
@@ -6,93 +6,93 @@ use App\Repositories\JenisBank\JenisBankRepository;
 
 class JenisBankService implements IJenisBankService
 {
-  private $JenisBankRepo;
+    private $jenisBankRepo;
 
-  public function __construct()
-  {
-    $this->JenisBankRepo = new JenisBankRepository();
-  }
-
-  private function _queryDataTable($search, $order)
-  {
-    $column_order = ['id', 'nama']; //field yang ada di table user
-    $column_search = ['id', 'nama']; //field yang diizin untuk pencarian
-    $order_by = ['id' => 'asc']; // default order
-
-    $query = $this->JenisBankRepo->getActive();
-
-    $query = datatableQuery($query, $search, $column_search, $column_order, $order, $order_by);
-
-    return $query;
-  }
-
-  public function getDataTable($request)
-  {
-    $search = $request->search;
-    $order = $request->order;
-    $length = $request->length;
-    $start = $request->start;
-    $draw = $request->draw;
-
-    $query = $this->_queryDataTable($search, $order);
-    if ($length != -1) {
-      $query = $query->limit($length, $start);
-    }
-    $query = $query->get();
-
-    $list = $query->getResult();
-    $countFilltered = $query->getNumRows();
-    $countAll = $this->JenisBankRepo->getActive()
-      ->get()
-      ->getNumRows();
-
-    foreach ($list as $i => $v) {
-      $start++;
-      $list[$i]->no = $start;
+    public function __construct()
+    {
+        $this->jenisBankRepo = new JenisBankRepository();
     }
 
-    $response = [
-      "draw" => $draw,
-      "recordsTotal" => $countFilltered,
-      "recordsFiltered" => $countAll,
-      "data" => $list,
-    ];
+    private function _queryDataTable($search, $order)
+    {
+        $column_order = ['id', 'nama']; //field yang ada di table user
+        $column_search = ['id', 'nama']; //field yang diizin untuk pencarian
+        $order_by = ['id' => 'asc']; // default order
 
+        $query = $this->jenisBankRepo->getActive();
 
-    return $response;
-  }
+        $query = datatableQuery($query, $search, $column_search, $column_order, $order, $order_by);
 
-  public function getJenisBanks()
-  {
-    return $this->JenisBankRepo->getActive()->get()->getResult();
-  }
-
-  public function saveData($data)
-  {
-    $message = 'Data berhasil disimpan';
-    if ((int)$data['id'] > 0) {
-      $message = 'Data berhasil diubah';
+        return $query;
     }
-    $this->JenisBankRepo->save($data);
 
-    $response = [
-      'status' => true,
-      'message' => $message
-    ];
+    public function getDataTable($request)
+    {
+        $search = $request->search;
+        $order = $request->order;
+        $length = $request->length;
+        $start = $request->start;
+        $draw = $request->draw;
 
-    return $response;
-  }
+        $query = $this->_queryDataTable($search, $order);
+        if ($length != -1) {
+            $query = $query->limit($length, $start);
+        }
+        $query = $query->get();
 
-  public function removeData($data)
-  {
-    $data['is_active'] = false;
-    $this->JenisBankRepo->save($data);
+        $list = $query->getResult();
+        $countFilltered = $query->getNumRows();
+        $countAll = $this->jenisBankRepo->getActive()
+            ->get()
+            ->getNumRows();
 
-    $response = [
-      'status' => true,
-      'message' => 'Data berhasil dihapus'
-    ];
+        foreach ($list as $i => $v) {
+            $start++;
+            $list[$i]->no = $start;
+        }
 
-    return $response;
-  }
+        $response = [
+        "draw" => $draw,
+        "recordsTotal" => $countFilltered,
+        "recordsFiltered" => $countAll,
+        "data" => $list,
+        ];
+
+
+        return $response;
+    }
+
+    public function getJenisBanks()
+    {
+        return $this->jenisBankRepo->getActive()->get()->getResult();
+    }
+
+    public function saveData($data)
+    {
+        $message = 'Data berhasil disimpan';
+        if ((int)$data['id'] > 0) {
+            $message = 'Data berhasil diubah';
+        }
+        $this->jenisBankRepo->save($data);
+
+        $response = [
+        'status' => true,
+        'message' => $message
+        ];
+
+        return $response;
+    }
+
+    public function removeData($data)
+    {
+        $data['is_active'] = false;
+        $this->jenisBankRepo->save($data);
+
+        $response = [
+        'status' => true,
+        'message' => 'Data berhasil dihapus'
+        ];
+
+        return $response;
+    }
 }
