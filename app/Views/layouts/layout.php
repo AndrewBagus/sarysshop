a0dda0d2be9eb910e3017b620613856b1547e324
Andrew Bagus <andrew.bagus@gmail.com>
2022-11-27 21:27:16 +0700

    disable loading page

--- app/Views/layouts/layout.php
+++ app/Views/layouts/layout.php
@@ -6,38 +6,38 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
   <meta http-equiv="content-type" content="text/html; charset=ISO-8859-1" />
-  <title><?= $title ?></title>
+  <title><?php echo $title ?></title>
 
-  <?= $this->include('layouts/css') ?>
+  <?php echo $this->include('layouts/css') ?>
 
-  <?= $this->renderSection('page-css') ?>
+  <?php echo $this->renderSection('page-css') ?>
 </head>
 
 <body class="app">
-  <div id="loader">
-    <div class="spinner"></div>
-  </div>
+  <!-- <div id="loader"> -->
+  <!--   <div class="spinner"></div> -->
+  <!-- </div> -->
   <div class="wrapper">
     <div>
-      <?= $this->include('layouts/sidebar') ?>
+      <?php echo $this->include('layouts/sidebar') ?>
       <div class="page-container">
-        <?= $this->include('layouts/navbar') ?>
+        <?php echo $this->include('layouts/navbar') ?>
         <main class="main-content bgc-grey-100">
           <!-- CSRF token -->
-          <?= csrf_field() ?>
+          <?php echo csrf_field() ?>
           <div id="mainContent">
-            <?= $this->renderSection('page-header') ?>
-            <?= $this->renderSection('page-content') ?>
+            <?php echo $this->renderSection('page-header') ?>
+            <?php echo $this->renderSection('page-content') ?>
           </div>
         </main>
         <footer class="bdT ta-c p-30 lh-0 fsz-sm c-grey-600"><span>Copyright © 2021 Designed by <a href="https://colorlib.com" target="_blank" title="Colorlib">Colorlib</a>. All rights reserved.</span></footer>
       </div>
     </div>
 
-    <?= $this->include('layouts/loading') ?>
-    <?= $this->include('layouts/js') ?>
+    <?php echo $this->include('layouts/loading') ?>
+    <?php echo $this->include('layouts/js') ?>
 
-    <?= $this->renderSection('page-js') ?>
+    <?php echo $this->renderSection('page-js') ?>
 </body>
 
 </html>
