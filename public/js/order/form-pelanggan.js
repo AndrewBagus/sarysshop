b479d72bb3ae4297aec6a73aa2b982a5012cb369
Andrew Bagus <andrew.bagus@gmail.com>
2022-12-07 18:56:34 +0700

    fix bug change alamat pelanggan n penerima

--- public/js/order/form-pelanggan.js
+++ public/js/order/form-pelanggan.js
@@ -1,57 +1,79 @@
 $(function () {
-  $('#pemesan').select2({
-    theme: 'bootstrap-5',
-    ajax: {
-      url: base_uri + '/pelanggan/findPelanggans',
-      type: 'POST',
-      dataType: 'json',
-      delay: 250,
-      data: function (params) {
-        const csrfName = $('[name=app_token_name]').attr('name') // CSRF Token name
-        const csrfHash = $('[name=app_token_name]').val() // CSRF hash
+  $('#pemesan')
+    .select2({
+      theme: 'bootstrap-5',
+      ajax: {
+        url: base_uri + '/pelanggan/findPelanggans',
+        type: 'POST',
+        dataType: 'json',
+        delay: 250,
+        data: function (params) {
+          const csrfName = $('[name=app_token_name]').attr('name') // CSRF Token name
+          const csrfHash = $('[name=app_token_name]').val() // CSRF hash
 
-        return {
-          [csrfName]: csrfHash,
-          search: params.term,
-        }
+          return {
+            [csrfName]: csrfHash,
+            search: params.term,
+          }
+        },
+        cache: true,
+        processResults: function (data, params) {
+          data = $.map(data, function (obj) {
+            obj.namaCari = obj.nama.replace(
+              new RegExp(params.term, 'gi'),
+              "<b style='color:#00bcd4'>$&</b>"
+            )
+            return obj
+          })
+          return {
+            results: data,
+          }
+        },
       },
-      cache: true,
-      processResults: function (data, params) {
-        data = $.map(data, function (obj) {
-          obj.namaCari = obj.nama.replace(
-            new RegExp(params.term, 'gi'),
-            "<b style='color:#00bcd4'>$&</b>"
-          )
-          return obj
+      minimumInputLength: 3,
+      placeholder: 'Cari Pemesan',
+      escapeMarkup: function (markup) {
+        return markup
+      },
+      templateResult: formatRepo,
+      templateSelection: formatRepoSelection,
+      language: {
+        inputTooShort: function (args) {
+          var remainingChars = args.minimum - args.input.length
+
+          var message = 'Masukan ' + remainingChars + ' atau lebih karakter'
+
+          return message
+        },
+        noResults: function () {
+          return 'Data tidak ditemukan'
+        },
+        searching: function () {
+          return 'Mencari...'
+        },
+      },
+    })
+    .on('select2:select', function (e) {
+      const repo = e.params.data
+      $('#kategori-pelanggan').val(repo.kategori_pelanggan_id)
+      const newOpt = new Option(repo.nama, repo.id, true, true)
+      const alamat = createAlamat(repo)
+      $('#pemesan-alamat').empty()
+      $('#pemesan-alamat').html(alamat)
+
+      if ($('#penerima-alamat').html() === '') {
+        $('#penerima').append(newOpt)
+        $('#penerima').trigger({
+          type: 'select2:select',
+          params: {
+            data: repo,
+          },
         })
-        return {
-          results: data,
-        }
-      },
-    },
-    minimumInputLength: 3,
-    placeholder: 'Cari Pemesan',
-    escapeMarkup: function (markup) {
-      return markup
-    },
-    templateResult: formatRepo,
-    templateSelection: formatRepoSelection,
-    language: {
-      inputTooShort: function (args) {
-        var remainingChars = args.minimum - args.input.length
-
-        var message = 'Masukan ' + remainingChars + ' atau lebih karakter'
-
-        return message
-      },
-      noResults: function () {
-        return 'Data tidak ditemukan'
-      },
-      searching: function () {
-        return 'Mencari...'
-      },
-    },
-  })
+        $('#penerima-alamat').empty()
+        $('#penerima-alamat').html(alamat)
+      }
+      refreshTableOrder()
+    })
 
   $(document).on('select2:open', '#pemesan', function () {
     document
@@ -82,25 +104,6 @@ $(function () {
   }
 
   function formatRepoSelection(repo) {
-    if (repo.nama !== undefined) {
-      $('#kategori-pelanggan').val(repo.kategori_pelanggan_id)
-      const newOpt = new Option(repo.nama, repo.id, true, true)
-      const alamat = alamat(repo)
-
-      $('#pemesan-alamat').html(alamat)
-      if ($('#penerima-alamat').html() === '') {
-        $('#penerima').append(newOpt)
-        $('#penerima').trigger({
-          type: 'select2:select',
-          params: {
-            data: repo,
-          },
-        })
-        $('#penerima-alamat').html(alamat)
-      }
-      refreshTableOrder()
-    }
-
     const nama =
       repo.is_default === '1'
         ? repo.nama
@@ -109,18 +112,3 @@ $(function () {
     return repo.text || nama
   }
 })
-
-function alamat(repo) {
-  const content = `<div class="col-xs-12 mt-2">
-        <div class="d-flex align-items-center justify-content-between position-relative">
-          <div class="d-flex align-items-center justify-content-between w-100">
-            <div style="font-size: 16px">
-              <div style="font-size: 16px; font-weight: bolder">Alamat: </div>
-              <small class="d-block">${repo.alamat}</small>
-              <small class="d-block">Kec. ${repo.kecamatan}, ${repo.kode_pos}</small>
-            </div>
-          </div>
-        </div>
-       </div>`
-  return content
-}
--- public/js/order/form-penerima.js
+++ public/js/order/form-penerima.js
@@ -1,57 +1,65 @@
 $(function () {
-  $('#penerima').select2({
-    theme: 'bootstrap-5',
-    ajax: {
-      url: base_uri + '/pelanggan/findPelanggans',
-      type: 'POST',
-      dataType: 'json',
-      delay: 250,
-      data: function (params) {
-        const csrfName = $('[name=app_token_name]').attr('name') // CSRF Token name
-        const csrfHash = $('[name=app_token_name]').val() // CSRF hash
+  $('#penerima')
+    .select2({
+      theme: 'bootstrap-5',
+      ajax: {
+        url: base_uri + '/pelanggan/findPelanggans',
+        type: 'POST',
+        dataType: 'json',
+        delay: 250,
+        data: function (params) {
+          const csrfName = $('[name=app_token_name]').attr('name') // CSRF Token name
+          const csrfHash = $('[name=app_token_name]').val() // CSRF hash
 
-        return {
-          [csrfName]: csrfHash,
-          search: params.term,
-        }
+          return {
+            [csrfName]: csrfHash,
+            search: params.term,
+          }
+        },
+        cache: true,
+        processResults: function (data, params) {
+          data = $.map(data, function (obj) {
+            obj.namaCari = obj.nama.replace(
+              new RegExp(params.term, 'gi'),
+              "<b style='color:#00bcd4'>$&</b>"
+            )
+            return obj
+          })
+          return {
+            results: data,
+          }
+        },
       },
-      cache: true,
-      processResults: function (data, params) {
-        data = $.map(data, function (obj) {
-          obj.namaCari = obj.nama.replace(
-            new RegExp(params.term, 'gi'),
-            "<b style='color:#00bcd4'>$&</b>"
-          )
-          return obj
-        })
-        return {
-          results: data,
-        }
+      minimumInputLength: 3,
+      placeholder: 'Cari Penerima',
+      escapeMarkup: function (markup) {
+        return markup
       },
-    },
-    minimumInputLength: 3,
-    placeholder: 'Cari Penerima',
-    escapeMarkup: function (markup) {
-      return markup
-    },
-    templateResult: formatRepo,
-    templateSelection: formatRepoSelection,
-    language: {
-      inputTooShort: function (args) {
-        var remainingChars = args.minimum - args.input.length
+      templateResult: formatRepo,
+      templateSelection: formatRepoSelection,
+      language: {
+        inputTooShort: function (args) {
+          var remainingChars = args.minimum - args.input.length
 
-        var message = 'Masukan ' + remainingChars + ' atau lebih karakter'
+          var message = 'Masukan ' + remainingChars + ' atau lebih karakter'
 
-        return message
+          return message
+        },
+        noResults: function () {
+          return 'Data tidak ditemukan'
+        },
+        searching: function () {
+          return 'Mencari...'
+        },
       },
-      noResults: function () {
-        return 'Data tidak ditemukan'
-      },
-      searching: function () {
-        return 'Mencari...'
-      },
-    },
-  })
+    })
+    .on('select2:select', function (e) {
+      const repo = e.params.data
+      const alamat = createAlamat(repo)
+
+      $('#penerima-alamat').empty()
+      $('#penerima-alamat').html(alamat)
+    })
 
   $(document).on('select2:open', '#penerima', function () {
     document
@@ -83,19 +91,6 @@ $(function () {
 
   function formatRepoSelection(repo) {
     if (repo.nama !== undefined) {
-      const alamat = `<div class="col-xs-12 mt-2">
-        <div class="d-flex align-items-center justify-content-between position-relative">
-          <div class="d-flex align-items-center justify-content-between w-100">
-            <div style="font-size: 16px">
-              <div style="font-size: 16px; font-weight: bolder">Alamat: </div>
-              <small class="d-block">${repo.alamat}</small>
-              <small class="d-block">Kec. ${repo.kecamatan}, ${repo.kode_pos}</small>
-            </div>
-          </div>
-        </div>
-       </div>`
-
-      $('#penerima-alamat').html(alamat)
     }
     return repo.text || repo.nama
   }
